3d23e803a694c1dfa5e1c7fa0e3add7d27c65dbf
Ignacio_Silva <nasho16101999@gmai.com>
2021-09-01 14:41:30 -0400

    era

--- index.js
+++ index.js
@@ -12,15 +12,12 @@ const WriteArch = (usuario, texto) => {
         console.log("Archivo Creado")
     })
 }
-
-
 const RellenoUsuarios = (user) => {
 
     var listaUsuarios = []
     listaUsuarios.push(user)
     return listaUsuarios
 }
-
 
 
 for (var i = 0; i < 4; i++) {
@@ -45,11 +42,9 @@ for (var i = 0; i < 4; i++) {
         //listaUsuarios.push(usuario)
 
         console.log(RellenoUsuarios(usuario));
-
         // Texto para escribir .txt 
-        let texto = `{nombre: ${nombre.first}, apellido: ${nombre.last}, id: ${uuidv4().slice(0,6)}, fecha: ${moment().format('MMM Do YYYY')}}`
+        //let texto = `{nombre: ${nombre.first}, apellido: ${nombre.last}, id: ${uuidv4().slice(0,6)}, fecha: ${moment().format('MMM Do YYYY')}}`
         //WriteArch("usuario.txt", texto)
-
         // Imprimir por consola el mismo txt 
         //console.log(chalk.blue.bgWhite.bold((`${texto}`)))
         
@@ -58,13 +53,46 @@ for (var i = 0; i < 4; i++) {
     .catch((e) => {
         console.log(e);
     });
-
-
-
-
  }
 
 
 
-    // falta hacer que entregue varios nombres, guardarlos en un objeto e imprimir el arreglo
+// falta hacer que entregue varios nombres, guardarlos en un objeto e imprimir el arreglo
+
+
+
+
+/*
+
+
+const GetUsers = (i) => {
+
+    for (var i = 0; i < 4; i++) {
+        axios
+        .get("https://randomuser.me/api/")
+        .then((data) => {
     
+            // Recibe Usuario
+            const nombre = data.data.results[0].name;
+            // Crea JSON con nombre de usuario
+            var usuario = {
+                nombre: `${nombre.first}`,
+                apellido: `${nombre.last}`,
+                id: `${uuidv4().slice(0,6)}`,
+                fecha: `${moment().format('MMM Do YYYY')}`
+            };
+            
+            console.log(usuario)
+            listaUsuarios.push(usuario)
+
+            return "aa"
+            
+        })
+        .catch((e) => {
+            console.log(e);
+        });
+    }
+}
+listaUsuarios = []
+console.log(GetUsers(5)) 
+*/
